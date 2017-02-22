4dd74a3d0ab5575c027b6b3e3c8c79f9efefc6ff
x-Sziklai <x-sziklaib@wizzair.com>
2017-02-22 16:18:44 +0100

    More scaffolding

--- Templates/Full/js/sketch.js
+++ Templates/Full/js/sketch.js
@@ -1,1 +1,19 @@
-sketch.js
+var settings = {
+	backgroundColor: '#1b1b1b'
+};
+
+function setup() {
+	// makes a resizable fluid canvas (utils)
+ 	pixelDensity(displayDensity());
+	Canvas.fluid(WEBGL);
+	background('#1b1b1b');
+}
+
+function update() {
+
+}
+
+function windowResized() {
+	// makes a resizable fluid canvas (utils)
+	Canvas.fluid();	
+}
